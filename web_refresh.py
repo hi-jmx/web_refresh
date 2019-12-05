3541f1dc1090d0e91f8a06716d6c93e0448cce12
hjmx <hi_helloming@163.com>
2019-12-05 16:02:14 +0800

    修改上个版本的bug

--- web_refresh.py
+++ web_refresh.py
@@ -5,6 +5,7 @@ from playsound import playsound
 from configparser import ConfigParser
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
+from selenium.common.exceptions import NoSuchElementException
 
 
 class My_driver:
@@ -14,6 +15,8 @@ class My_driver:
     def __init__(self, web_driver):
         self.web_driver = web_driver
         self.tag_url = ''
+        self.s_role = ''
+
     def login(self, url, user_name, pswd):
         self.web_driver.get(url)
         self.web_driver.find_element_by_id('J_GlobalTop').click()
@@ -22,15 +25,24 @@ class My_driver:
 
     def find_item(self, url, number):
         self.web_driver.get(url)
+        # / html / body / div[4] / div[2] / div[4] / div[3] / div[2] / div / a[1]
+        # /html/body/div[4]/div[2]/div[4]/div[3]/div[2]/div/a[4]/span
+        # /html/body/div[4]/div[2]/div[4]/div[3]/div[2]/div/a[4]
         startIndex = self.web_driver.page_source.find('拍卖交易')
+        if -1 == startIndex:
+            print('go the next page')
+            self.web_driver.get(url.replace('0.shtml', '2.shtml'))
+            startIndex = self.web_driver.page_source.find('拍卖交易')
         endIndex = self.web_driver.page_source.find('</div>', startIndex)
         targetUrl = self.web_driver.page_source[startIndex:endIndex]
+
         print(targetUrl)
         try:
             money_m = re.search('<li><b>1元=(.*?)</b>', targetUrl).group(1)
             url_m = re.search('<li class="btn_box"><a class="btnlink_o_s_small" href="(.*?)" ', targetUrl).group(1)
         except:
             print('find money or url may error!')
+
             return
 
         print('price:{}'.format(money_m))
@@ -38,13 +50,24 @@ class My_driver:
         print('number:{}'.format(number))
         if float(money_m) > float(number):
             self.tag_url = url_m.replace('&amp;', '&')
+        else:
+            self.tag_url = ''
 
     def check(self, target_url):
         self.web_driver.get(target_url)
         try:
-            self.web_driver.find_element_by_id('divNotBuy')
+            self.web_driver.switch_to_alert().accept()
             return True
         except:
+            pass
+        try:
+            self.web_driver.find_element_by_id('divNotBuy')
+            print('can not bug')
+            return True
+        # except NoSuchElementException:
+        #     self.web_driver.find_element_by_id('linkCreateOrder').click
+        #     return False
+        except NoSuchElementException:
             return False
 
     def fill_area(self, area):
@@ -98,6 +121,7 @@ class My_driver:
             s_qq = self.web_driver.find_element_by_id('PurchaseOrderNew1_txtBuyerQQ')
         except:
             print('qq:may something error!')
+            s_qq = self.web_driver.find_element_by_id('txtQq')
         if s_qq:
             s_qq.send_keys(qq)
             return True
@@ -110,33 +134,55 @@ class My_driver:
         except:
             self.web_driver.find_element_by_name('txtReceivingRole').send_keys('1')
 
+    def find_role(self):
+        # /html/body/form/div[2]/div[2]/div[3]/div[2]/div[1]/div[7]/div/p[1]/input
+        try:
+            s_role = self.web_driver.find_element_by_xpath('/html/body/form/div[2]/div[2]/div[3]/div[2]/div[1]/div[7]/div/p[1]/input')
+        except:
+            # / html / body / form / div[3] / div[2] / div[2] / div[1] / div[5] / div / p[1] / input
+            s_role = self.web_driver.find_element_by_id('rdNewGameId0')
+        if s_role:
+            self.s_role = s_role
+            return True
+        return False
+
+
     def fill_info(self, target_url, area, server, name, qq):
         if self.check(target_url):
             return False
+        # try:
+        #     if self.fill_area(area):
+        #         self.fill_server(server)
+        #         self.fill_name(name)
+        #         self.fill_name2(name)
+        #         self.fill_role()
+        #
+        #
+        # except:
+        #     #driver.find_element_by_id('BuyerGameRoleInfo_rbExistGameRole_0').click()
+        #     print('try find BuyerGameRoleInfo_rbExistGameRole_0!')
+        #     # s_role = self.web_driver.find_element_by_id('BuyerGameRoleInfo_rbExistGameRole_0')
+        #     if self.find_role():
+        #         self.s_role.click
+        #
+        #         #assert(True)
+        #         # print('may some error!')
+        #         # return False
         try:
-            self.fill_area(area)
-            self.fill_server(server)
-            self.fill_name(name)
-            self.fill_name2(name)
+            self.fill_role()
         except:
-            #driver.find_element_by_id('BuyerGameRoleInfo_rbExistGameRole_0').click()
-            print('try find BuyerGameRoleInfo_rbExistGameRole_0!')
-            s_role = self.web_driver.find_element_by_id('BuyerGameRoleInfo_rbExistGameRole_0')
-            if s_role:
-                print('s_role is found!')
-                s_role.click()
-
-            #assert(True)
-            # print('may some error!')
-            # return False
-        self.fill_role()
-
+            pass
         # 选择客服
         self.web_driver.find_element_by_xpath('(//*[@id="kf_list"]/li)[last()]').click()
-
-        self.web_driver.find_element_by_id('PurchaseOrderNew1_rdNoPostSale').click()
+        try:
+            self.web_driver.find_element_by_id('PurchaseOrderNew1_rdNoPostSale').click()
+        except:
+            self.web_driver.find_element_by_id('rdbtnOffPostSaleIndemnity').click()
         self.fill_qq(qq)
-        self.web_driver.find_element_by_id('lbtnCreateOrder').click()
+        try:
+            self.web_driver.find_element_by_id('lbtnCreateOrder').click()
+        except:
+            self.web_driver.find_element_by_id('linkOk').click()
         return True
 
     def handle(self, url, number, area, server, name, qq):
@@ -147,7 +193,12 @@ class My_driver:
                 if self.fill_info(parser.tag_url, area, server, name, qq):
                     # 提醒
                     playsound('1.mp3')
+                    self.tag_url = ''
+                    self.s_role = ''
                     break
+                else:
+                    continue
+
 
     def handler_adaptor(self, fun, my_url, number, area, server, name, qq):
         return lambda event: fun(my_url, number, area, server, name, qq)
